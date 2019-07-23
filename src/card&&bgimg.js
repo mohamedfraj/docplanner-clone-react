75846aa8f5de926301014f3b8b10a86d1d96979f
unknown <fraj.mohamed97@gmail.com>
2019-07-23 15:58:02 +0100

    retouches

--- src/card&&bgimg.js
+++ src/card&&bgimg.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import bg from './bg.png'
 
 const divStyle = {
@@ -13,17 +13,17 @@ const tab = [
         logo: 'https://www.docplanner.com/img/flag.png',
         titre: "Leader in 10 countries",
         paragraphdiv: "Poland, Turkey, Spain, Italy, Czech Republic, Mexico, Brazil, Colombia, Argentina and Chile",
-        css:"h"
+        css: "h"
     }, {
         logo: "https://www.docplanner.com/img/visits.png",
         titre: "1.5 million appointments",
         paragraphdiv: "booked last month",
-        
+
     }, {
         logo: "https://www.docplanner.com/img/patients.png",
         titre: "30 million unique patients",
         paragraphdiv: "visit us every month",
-        css:"h"
+        css: "h"
     }, {
         logo: "https://www.docplanner.com/img/doctors.png ",
         titre: "2 million active doctors",
@@ -32,24 +32,30 @@ const tab = [
 ];
 
 function Cardimg() {
-    return (<div style={divStyle}>
-        <Container className='Containercards'>
-            <div className='titre-parag'>
-                <h1 className='H1the-world'>The world's biggest healthcare platform</h1>
-                <p className='paragraph-wework' >We work from 6 offices all over the world, bringing Docplanner Group to life in almost 20 countries.</p>
-            </div>
-            <div className='descardes'>
-                {tab.map(el => 
-                
-                    <div className='cardlogo' style={{margin: el.css=="h" && '40px' }}>
-                        <div className='centre'>
-                            <img src={el.logo}></img>
-                        </div>
-                        <h2 className='tith1 centre'>{el.titre}</h2>
-                        <p className='paragpoland centre' >{el.paragraphdiv}</p>
-                    </div>)}
-            </div>
-        </Container>
-    </div >)
+    return (
+        <div style={divStyle}>
+            <Container className='Containercards'>
+                <Row>
+                <Col lg={5} md={12} sm={12}>
+                <div className='titre-parag'>
+                    <h1 className='H1the-world'>The world's biggest healthcare platform</h1>
+                    <p className='paragraph-wework' >We work from 6 offices all over the world, bringing Docplanner Group to life in almost 20 countries.</p>
+                </div>
+                </Col>
+                <Col lg={7} md={12} sm={12}>
+                <div className='descardes'>
+                    {tab.map(el =>
+
+                        <div className='cardlogo' style={{ marginTop: el.css == "h" && '40px' }}>
+                            <div className='centre'>
+                                <img src={el.logo}></img>
+                            </div>
+                            <h2 className='tith1 centre'>{el.titre}</h2>
+                            <p className='paragpoland centre' >{el.paragraphdiv}</p>
+                        </div>)} 
+                </div> </Col>
+                </Row>
+            </Container>
+        </div >)
 }
 export default Cardimg;
