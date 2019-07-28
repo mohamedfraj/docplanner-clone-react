eb5ce3c1f2bc1bfe2c8f178d86cc59fae6c56cc7
unknown <fraj.mohamed97@gmail.com>
2019-07-28 01:58:31 +0100

    Updates

---
+++ precache-manifest.c179bbeef164cc913d7314c6da65d890.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bfcaad827a8b9a2780e5f7279f527ff",
+    "url": "/docplanner-clone-react/index.html"
+  },
+  {
+    "revision": "ecd1cce211ba301db34b",
+    "url": "/docplanner-clone-react/static/css/main.4e1194c5.chunk.css"
+  },
+  {
+    "revision": "29843aee9e111073d5b4",
+    "url": "/docplanner-clone-react/static/js/2.e14fca06.chunk.js"
+  },
+  {
+    "revision": "ecd1cce211ba301db34b",
+    "url": "/docplanner-clone-react/static/js/main.12663c07.chunk.js"
+  },
+  {
+    "revision": "109b3a1d7df2a5198832",
+    "url": "/docplanner-clone-react/static/js/runtime~main.2f5682f2.js"
+  },
+  {
+    "revision": "c29854acac38fdc1c343e5ab45e1e054",
+    "url": "/docplanner-clone-react/static/media/bg.c29854ac.png"
+  }
+]);
